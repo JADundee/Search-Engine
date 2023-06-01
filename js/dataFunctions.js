f407f565c8bb4f3b19250060c544d3aa304262c1
JADundee <Jadundee@icloud.com>
2023-06-01 07:11:22 -0600

    Added dataFunctions

---
+++ js/dataFunctions.js
@@ -0,0 +1,42 @@
+export const getSearchTerm = () => {
+    const rawSearchTerm = document.getElementById("search").value.trim();
+    const regex = /[ ]{2,}/gi;
+    const searchTerm = rawSearchTerm.replaceAll(regex, " ");
+    return searchTerm;
+}
+
+export const retrieveSearchResults = async (searchTerm) => {
+    const wikiSearchString = getWikiSearchString(searchTerm);
+    const wikiSearchResults = await requestData(wikiSearchString);
+    let resultArray = [];
+    if (wikiSearchResults.hasOwnProperty("query")) {
+        resultArray = processWikiResults(wikiSearchResults.query.pages);
+    }
+    return resultArray;
+}
+
+const getWikiSearchString = (searchTerm) => {
+    const maxCharacters = getMaxCharacters();
+    const rawSearchString = `https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${searchTerm}&gsrlimit=20&prop=pageimages|extracts&exchars=${maxCharacters}&exintro&explaintext&exlimit=max&format=json&origin=*`;
+    const searchString = encodeURI(rawSearchString);
+    return searchString;
+}
+
+const getMaxCharacters = () => {
+    const width = window.innerWidth || document.body.clientWidth;
+    let maxCharacters;
+    if (width < 414) maxCharacters = 65;
+    if (width >= 414 && width < 1400) maxCharacters = 100;
+    if (width >= 1400) maxCharacters = 130;
+    return maxCharacters;
+}
+
+const requestData = async (searchString) => {
+    try {
+        const response = await fetch(searchString);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error(err)
+    }
+}
