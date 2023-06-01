d540c42d55af87485ba1092b24dd9c6894d735d2
JADundee <Jadundee@icloud.com>
2023-06-01 07:57:02 -0600

    Added setStatsLine()

--- js/searchResults.js
+++ js/searchResults.js
@@ -52,3 +52,12 @@ const createResultText = (result) => {
 export const clearStatsLine = () => {
     document.getElementById("stats").textContent = "";
 }
+
+export const setStatsLine = (numberOfResults) => {
+    const statsLine = document.getElementById("stats");
+    if (numberOfResults) {
+        statsLine.textContent= `Displaying ${numberOfResults} results.`
+    } else {
+        setStatsLine.textContent = "Sorry, no results matched your search."
+    }
+}
